11941d5168745327da6d51c1d3056395544bbac0
MarkZhuo <markzhuo1@163.com>
2024-02-06 16:37:53 +0800

    feat: tradeMark AddOrUpdate

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,9 +1,19 @@
 import request from '@/utils/request';
-import type { TradeMarkResponseData } from '@/api/product/trademark/type';
+import type { TradeMarkResponseData, TradeMark } from '@/api/product/trademark/type';
 
 enum API {
-  TRADEMARK_URL = '/admin/product/baseTrademark/'
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
+  ADD_TRADEMARK_URL = '/admin/product/baseTrademark/save',
+  UPDATE_TRADEMARK_URL = '/admin/product/baseTrademark/update'
 }
 
 export const reqHasTrademark = (currentPage: number, pageSize: number) =>
   request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${currentPage}/${pageSize}`);
+
+export const reqAddOrUpdateTradeMark = (data: TradeMark) => {
+  if (data.id) {
+    return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data);
+  } else {
+    return request.post<any, any>(API.ADD_TRADEMARK_URL, data);
+  }
+};
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { reqHasTrademark } from '@/api/product/trademark';
-import type { Records, TradeMarkResponseData } from '@/api/product/trademark/type';
+import { ref, onMounted, reactive } from 'vue';
+import { reqHasTrademark, reqAddOrUpdateTradeMark } from '@/api/product/trademark';
+import type { Records, TradeMarkResponseData, TradeMark } from '@/api/product/trademark/type';
+import { ElMessage, type UploadProps } from 'element-plus';
 
 const currentPage = ref<number>(1);
 const pageSize = ref<number>(5);
 const pageSizes = [5, 10, 15, 20];
 const total = ref<number>(0);
 const tableData = ref<Records>([]);
-const dialogFormVisible = ref<boolean>(true);
+const dialogFormVisible = ref<boolean>(false);
+const tradeMarkParams = reactive<TradeMark>({
+  tmName: '',
+  logoUrl: ''
+});
 
 onMounted(() => {
   getHasTrademark();
@@ -16,7 +21,6 @@ onMounted(() => {
 
 const getHasTrademark = async () => {
   const result: TradeMarkResponseData = await reqHasTrademark(currentPage.value, pageSize.value);
-  console.log(result);
   if (result.code === 200) {
     total.value = result.data.total;
     tableData.value = result.data.records;
@@ -30,10 +34,68 @@ const handleSizeChange = () => {
 
 const addTradeMark = () => {
   dialogFormVisible.value = true;
+  Object.keys(tradeMarkParams).forEach((o) => {
+    tradeMarkParams[o] = '';
+  });
 };
 
-const updateTradeMark = () => {
+const updateTradeMark = (row: TradeMark) => {
   dialogFormVisible.value = true;
+  Object.assign(tradeMarkParams, row);
+};
+
+const cancel = () => {
+  dialogFormVisible.value = false;
+};
+
+const confirm = async () => {
+  const result: any = await reqAddOrUpdateTradeMark(tradeMarkParams);
+  if (result.code === 200) {
+    ElMessage({
+      type: 'success',
+      message: `${tradeMarkParams?.id ? '修改' : '添加'}品牌成功`
+    });
+    dialogFormVisible.value = false;
+    if (!tradeMarkParams?.id) {
+      currentPage.value = 1;
+    }
+    await getHasTrademark();
+  } else {
+    ElMessage({
+      type: 'error',
+      message: `${tradeMarkParams?.id ? '修改' : '添加'}品牌失败`
+    });
+    dialogFormVisible.value = false;
+  }
+};
+
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  if (
+    rawFile.type == 'image/jpeg' ||
+    rawFile.type == 'image/jpg' ||
+    rawFile.type == 'image/png' ||
+    rawFile.type == 'image/gif'
+  ) {
+    if (rawFile.size / (1024 * 1024) < 4) {
+      return true;
+    } else {
+      ElMessage({
+        type: 'error',
+        message: '上传图片大小必须小于4M'
+      });
+      return false;
+    }
+  } else {
+    ElMessage({
+      type: 'error',
+      message: '上传图片必须是jpeg｜jpg｜png｜gif'
+    });
+    return false;
+  }
+};
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (response, uploadFile) => {
+  tradeMarkParams.logoUrl = response.data;
 };
 </script>
 
@@ -53,7 +115,12 @@ const updateTradeMark = () => {
         </el-table-column>
         <el-table-column prop="address" label="品牌操作">
           <template #="{ row, $index }">
-            <el-button type="primary" size="small" icon="Edit" @click="updateTradeMark"></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              @click="updateTradeMark(row)"
+            ></el-button>
             <el-button type="primary" size="small" icon="Delete"></el-button>
           </template>
         </el-table-column>
@@ -70,8 +137,59 @@ const updateTradeMark = () => {
         @current-change="getHasTrademark"
       />
     </el-card>
-    <el-dialog v-model="dialogFormVisible" title="添加"></el-dialog>
+    <el-dialog v-model="dialogFormVisible" :title="`${tradeMarkParams?.id ? '修改' : '添加'}品牌`">
+      <el-form style="width: 80%" label-width="100px">
+        <el-form-item label="品牌名称">
+          <el-input v-model="tradeMarkParams.tmName" placeholder="请输入品牌名称" />
+        </el-form-item>
+        <el-form-item label="品牌LOGO">
+          <el-upload
+            class="avatar-uploader"
+            action="/api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="tradeMarkParams.logoUrl" :src="tradeMarkParams.logoUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" size="default" @click="cancel">取消</el-button>
+        <el-button type="primary" size="default" @click="confirm">确定</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
